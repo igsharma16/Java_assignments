bfd27aa95b670f8dd7ef252a58b0002dfc60b4fa
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-10-15 19:43:35 +0530

    Create Even_Elements.java

---
+++ Java Assignments/Java Array Progams/Even_Elements.java
@@ -0,0 +1,13 @@
+public class Even_Elements {
+    public static void main(String[] args){
+        String[] arr = {"Hindi" , "Odissi" , "Malyalam" , "Tamil" , "Assammese" , "Santhali" ,"Urdu" ,"Kannada" , "Marathi" , "Telugu" , "Gujarati" , "Punjabi" ,"Sindhi" , "Manipuri"};
+        //Printing elements in reverse
+       for(int i = 0 ; i < arr.length ; i++ ){
+        if( i % 2 == 0){
+            System.out.println(arr[i]);
+        }
+        }
+    }
+}
+// TO PRINT ELEMENTS OF ARRAY AT EVEN POSITIONS
+//By Gulshan Sharma
