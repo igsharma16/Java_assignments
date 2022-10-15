043c371b68711f855658484e79c62b8d7aea9518
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-10-15 19:45:23 +0530

    Odd positioned Elements

---
+++ Java Assignments/Java Array Progams/Odd_Elements.java
@@ -0,0 +1,14 @@
+public class code {
+    public static void main(String[] args){
+        String[] arr = {"Hindi" , "Odissi" , "Malyalam" , "Tamil" , "Assammese" , "Santhali" ,"Urdu" ,"Kannada" , "Marathi" , "Telugu" , "Gujarati" , "Punjabi" ,"Sindhi" , "Manipuri"};
+        //Printing elements in reverse
+       for(int i = 0 ; i < arr.length ; i++ ){
+        if( i % 2 != 0){
+            System.out.println(arr[i]);
+        }
+            
+        }
+    }
+}
+// TO PRINT ELEMENTS OF ARRAY AT ODD POSITIONS
+//By Gulshan Sharma
