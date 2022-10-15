38a3cfdecf7b00a9d75a409abc5aa9ade6b962e0
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-10-15 19:31:44 +0530

    Create Array_reverse.java

---
+++ Java Assignments/Java Array Progams/Array_reverse.java
@@ -0,0 +1,10 @@
+public class Array_reverse {
+    public static void main(String[] args){
+        String[] arr = {"India" , "Australia" , "Japan" , "Argentina" , "Brazil" , "Madagascar"};
+        //Printing elements in reverse
+       for(int i = arr.length - 1 ; i >= 0 ; i-- ){
+            System.out.println(arr[i]);
+        }
+    }
+}
+// TO PRINT ELEMENTS OF ARRAY IN REVERSE
