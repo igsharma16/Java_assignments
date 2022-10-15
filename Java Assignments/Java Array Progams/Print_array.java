9dd3552499040100650f0e0a4a727991556fe555
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-10-15 19:27:39 +0530

    Create Print_array.java

---
+++ Java Assignments/Java Array Progams/Print_array.java
@@ -0,0 +1,12 @@
+public class Print_array {
+    public static void main(String[] args){
+        String[] arr = {"India" , "Australia" , "Japan" , "Argentina" , "Brazil" , "Madagascar"}
+        //Printing elements
+
+        for(String i : arr){
+            System.out.println(i);
+        }
+    }
+}
+
+// TO PRINT ELEMENTS OF ARRAY
