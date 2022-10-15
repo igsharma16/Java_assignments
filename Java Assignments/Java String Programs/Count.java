44e5b7eb647a0a6c722afb4e772aaac3c3999006
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-10-15 15:17:37 +0530

    Add files via upload

---
+++ Java Assignments/Java String Programs/Count.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Count {
+    public static void main(String[] args){
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str = ip.nextLine();
+        System.out.println("The number of characters in the string are :\t" + str.length());
+
+    }// By Gulshan sharma
+}
