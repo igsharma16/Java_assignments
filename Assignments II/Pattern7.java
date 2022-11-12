8bd75e0d409d99017dc9808512a3110b3abe4d89
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-11-12 15:23:47 +0530

    7th Que

---
+++ Assignments II/Pattern7.java
@@ -0,0 +1,14 @@
+public class Pattern {
+    public static void main(String[] args){
+            for(int i = 1 ; i < 6 ; i++){
+                for(int j = 1 ; j < 6 ; j++){
+                    if(i == j){
+                        System.out.print(i);
+                    }
+                    System.out.print(1);
+                }
+                System.out.println();
+            }
+        }   
+}
+
