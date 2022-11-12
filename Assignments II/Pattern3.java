03b45c2dafef6f37c0551e1c57c0ee0ffefbb836
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-11-12 14:59:29 +0530

    Create Pattern3.java

---
+++ Assignments II/Pattern3.java
@@ -0,0 +1,15 @@
+//    GULSHAN SHARMA
+public class Pattern {
+    public static void main(String[] args){
+        for(int i = 0 ; i < 10 ; i++){
+            int j = 10;
+            while (j > i) {
+                System.out.print("*");
+                j--;
+            }
+            System.out.println();
+        }
+    }
+}
+
+//12 Nov 2022 M. Ele.
