a130f678eeee29d1673ca84fbe515b999eb66245
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-11-12 15:22:51 +0530

    8th Que

---
+++ Assignments II/Pattern8.java
@@ -0,0 +1,10 @@
+public class Pattern{
+    public static void main(String[] args){
+        for(int i = 0 ; i<5 ; i++){
+            System.out.println();
+            for(int j = 1 ; j < 6 ; j++){
+                System.out.print(j);
+            }
+        }
+    }
+}
