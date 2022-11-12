058f77eb522e37d389665fb34f9d4d9be6917537
GULSHAN SHARMA <115257669+igsharma16@users.noreply.github.com>
2022-11-12 15:12:17 +0530

    Create Pattern5.java

---
+++ Assignments II/Pattern5.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class Pattern {
+    public static void main(String[] args){
+            int[] a1 = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10};
+            for(int i = 1 ; i < 10 ; i++){
+                for(int j = 0 ; j < i ; j++){
+                    System.out.print(a1[j + " "]);
+                }
+                System.out.println();
+            }
+        }   
+}
+//By Gulshan Sharma
